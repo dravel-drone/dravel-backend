4d56147b3e2f1110d5a8dae2ff6a296d00d011a4
Damin Lee <ldm0830@gmail.com>
2024-07-17 23:12:59 +0900

    DRAV-37 feat: Term 스키마 추가

--- schemas.py
+++ schemas.py
@@ -1,15 +1,17 @@
-from pydantic import BaseModel, EmailStr
-from datetime import date
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
 
-from typing import Optional
 
-# pydantic 모델 추가
-
-class UserTest(BaseModel):
-    uid: str
-    name: str
-    is_admin: bool
-    drone: Optional[str]
-
+# 약관(Term) pydantic 스키마
+class TermBase(BaseModel):
+    title: str
+    content: str
+    require: int
+class TermCreate(TermBase):
+    pass
+class Term(TermBase):
+    id: int
     class Config:
         orm_mode = True
+
