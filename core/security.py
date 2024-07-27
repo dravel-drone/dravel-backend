43cbf7e333787c504bf0f4306221b7116c752053
oseoko <oseoko@naver.com>
2024-07-27 23:04:42 +0900

    DRAV-101 Feat: access token 생성 함수 추가

---
+++ core/security.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+from typing import Optional
+import jwt
+from core.config import settings
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None)-> tuple[str, datetime]:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.ACCESS_SECRET_KEY, algorithm=settings.ACCESS_TOKEN_ENCODE_ALGORITHM)
+    return encoded_jwt, expire
+
