51f5c2776ed3bbc24610d599e8b257382f8df5c7
oseoko <oseoko@naver.com>
2024-07-28 00:20:38 +0900

    DRAV-101 Feat: 비밀번호 검증 함수 추가

--- core/security.py
+++ core/security.py
@@ -1,9 +1,14 @@
 from datetime import datetime, timedelta
 from typing import Optional
 import jwt
+from fastapi import HTTPException, status
+from passlib.context import CryptContext
 from core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password + settings.PASSWORD_SALT, hashed_password)
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None)-> tuple[str, datetime]:
     to_encode = data.copy()
