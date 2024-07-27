235238983bb615a31dd177e87bae5f05fde8f2d5
oseoko <oseoko@naver.com>
2024-07-27 23:04:56 +0900

    DRAV-101 Feat: refresh token 생성 함수 추가

--- core/security.py
+++ core/security.py
@@ -13,3 +13,13 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None)->
     encoded_jwt = jwt.encode(to_encode, settings.ACCESS_SECRET_KEY, algorithm=settings.ACCESS_TOKEN_ENCODE_ALGORITHM)
     return encoded_jwt, expire
 
+def create_refresh_token(data: dict, expires_delta: Optional[timedelta] = None) -> tuple[str, datetime]:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=settings.REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.REFRESH_SECRET_KEY, algorithm=settings.REFRESH_TOKEN_ENCODE_ALGORITHM)
+    return encoded_jwt, expire
