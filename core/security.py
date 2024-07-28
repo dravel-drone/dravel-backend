7d719d6f783f85ea1920029231d7d3915ddb800f
Damin Lee <ldm0830@gmail.com>
2024-07-28 09:58:15 +0900

    DRAV-51 feat: 추출된 토큰 인증하는 함수

--- core/security.py
+++ core/security.py
@@ -25,3 +25,10 @@ def get_access_key_from_token(token: str) -> str:
     except JWTError as e:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid token: {e}")
 
+
+async def get_access_key_from_request(request: Request) -> str:
+    token = get_token_from_header(request)
+    if token is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization header가 없거나 invalid함")
+    return get_access_key_from_token(token)
+
