27284db261b9967e130ac8bbca003e1a35435718
Damin Lee <ldm0830@gmail.com>
2024-07-29 23:56:41 +0900

    DRAV-51 feat: payload 정보 추출 함수 추가

--- core/security.py
+++ core/security.py
@@ -2,3 +2,29 @@ from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_payload_info(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = decode_access_token(token)
+        if payload is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid authentication credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        encoded_jwt = payload.get("encoded_jwt")
+        expire = payload.get("expire")
+        if encoded_jwt is None or expire is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid authentication credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return encoded_jwt, expire
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
