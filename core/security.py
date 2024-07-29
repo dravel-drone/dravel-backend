01d53c3bd7faa8a7679eff378999ae9296f1e466
Damin Lee <ldm0830@gmail.com>
2024-07-29 23:54:54 +0900

    DRAV-51 fix: 헤더에서 추출하는 코드 수정

--- core/security.py
+++ core/security.py
@@ -1,34 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException, status, Depends
-from jose import JWTError, jwt
-from typing import Optional
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 
-app = FastAPI()
-
-SECRET_KEY = "dlatlzl"  # 테스트용 임시키
-ALGORITHM = "HS256"
-
-
-def get_token_from_header(request: Request) -> Optional[str]:
-    auth_header = request.headers.get("Authorization")
-    if auth_header and auth_header.startswith("Bearer "):
-        return auth_header[len("Bearer "):]
-    return None
-
-
-def get_access_key_from_token(token: str) -> str:
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        access_key = payload.get("access")
-        if access_key is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="'access'정보가 없음")
-        return access_key
-    except JWTError as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid token: {e}")
-
-
-async def get_access_key_from_request(request: Request) -> str:
-    token = get_token_from_header(request)
-    if token is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization header가 없거나 invalid함")
-    return get_access_key_from_token(token)
-
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
