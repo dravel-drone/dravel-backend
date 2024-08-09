6ec40f22871ad468bae7d93cdb0edec80dbb6264
Damin Lee <ldm0830@gmail.com>
2024-08-10 04:58:03 +0900

    DRAV-108 feat: 아이디 중복 검사 로직 추가

--- api/v1/endpoints/register.py
+++ api/v1/endpoints/register.py
@@ -19,6 +19,10 @@ def create_term(
         user: UserCreate,
         db: Session = Depends(get_db)
 ):
+    # 아이디 중복 검사
+    existing_id = db.query(UserModel).filter(UserModel.id == user.id).first()
+    if existing_id:
+        raise HTTPException(status_code=400, detail="이미 있는 아이디입니다.")
 
     hashed_password = get_password_hash(user.password)
     user.password = hashed_password
