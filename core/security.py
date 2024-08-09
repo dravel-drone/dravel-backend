6536b090ec9dd442b1f6628b32d5fbc850ce70fb
Damin Lee <ldm0830@gmail.com>
2024-08-10 04:55:19 +0900

    DRAV-108 fix: 안쓰는 코드 일단 주석처리

--- core/security.py
+++ core/security.py
@@ -6,34 +6,34 @@ from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from core.config import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-async def get_payload_info(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = decode_access_token(token)
-        if payload is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid authentication credentials",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-        encoded_jwt = payload.get("encoded_jwt")
-        expire = payload.get("expire")
-        if encoded_jwt is None or expire is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid authentication credentials",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-        return encoded_jwt, expire
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+# async def get_payload_info(token: str = Depends(oauth2_scheme)):
+#     try:
+#         payload = decode_access_token(token)
+#         if payload is None:
+#             raise HTTPException(
+#                 status_code=status.HTTP_401_UNAUTHORIZED,
+#                 detail="Invalid authentication credentials",
+#                 headers={"WWW-Authenticate": "Bearer"},
+#             )
+#         encoded_jwt = payload.get("encoded_jwt")
+#         expire = payload.get("expire")
+#         if encoded_jwt is None or expire is None:
+#             raise HTTPException(
+#                 status_code=status.HTTP_401_UNAUTHORIZED,
+#                 detail="Invalid authentication credentials",
+#                 headers={"WWW-Authenticate": "Bearer"},
+#             )
+#         return encoded_jwt, expire
+#     except Exception as e:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Invalid authentication credentials",
+#             headers={"WWW-Authenticate": "Bearer"},
+#         )
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password + settings.PASSWORD_SALT, hashed_password)
