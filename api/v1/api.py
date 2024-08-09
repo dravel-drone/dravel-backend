bc2b22061162eb093f31f58d38b2c1d77e95e55f
oseoko <oseoko@naver.com>
2024-08-10 02:10:15 +0900

    DRAV-81 feat: router 연결

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints import test
+from api.v1.endpoints import test, terms
 
-api_router = APIRouter()
-api_router.include_router(test.router, prefix="/test", tags=["test"])
+# api_router = APIRouter()
+# api_router.include_router(test.router, prefix="/test", tags=["test"])
+
+router = APIRouter()
+router.include_router(terms.router)
