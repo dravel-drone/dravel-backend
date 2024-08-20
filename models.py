e8309fef3d5235131a6f0389890ea8038bf178fa
oseoko <oseoko@naver.com>
2024-08-20 17:01:46 +0900

    DRAV-15 feat: 모델에 trend_dronespot table 추가

--- models.py
+++ models.py
@@ -85,6 +85,7 @@ class Dronespot(Base):
     user_dronespot_likes = relationship('UserDronespotLike', back_populates='dronespot')
     reviews = relationship('Review', back_populates='dronespot')
     course_visits = relationship('CourseVisit', back_populates='dronespot')
+    trend_dronespots = relationship('TrendDronespot', back_populates='dronespot')
 
 class UserDronespotLike(Base):
     __tablename__ = 'user_dronespot_like'
@@ -94,6 +95,15 @@ class UserDronespotLike(Base):
 
     user = relationship('User', back_populates='user_dronespot_likes')
     dronespot = relationship('Dronespot', back_populates='user_dronespot_likes')
+
+class TrendDronespot(Base):
+    __tablename__ = 'trend_dronespot'
+
+    dronespot_id = Column(INTEGER(unsigned=True), ForeignKey('dronespot.id'), primary_key=True, nullable=False)
+    count = Column(INTEGER(unsigned=True), nullable=False)
+
+    dronespot = relationship('Dronespot',back_populates='trend_dronespots')
+
 
 class Review(Base):
     __tablename__ = 'review'
