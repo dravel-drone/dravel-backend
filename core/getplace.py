829d9fef577c5324aa7d5ec91cd6d3122ef33acf
Damin Lee <ldm0830@gmail.com>
2024-08-24 16:08:40 +0900

    DRAV-113 feat: 주변 장소 정보 가져오는 함수 구현

--- core/getplace.py
+++ core/getplace.py
@@ -4,15 +4,11 @@ import xmltodict
 from fastapi import Depends
 from pydantic import BaseModel
 from typing import Dict, Any
-from models import Dronespot as DronespotModel, Place as PlaceModel
 
+from models import Dronespot as DronespotModel, Place as PlaceModel, DronePlace as DronePlaceModel
 from sqlalchemy.orm import Session
-
 from core.config import settings
-import json
-
 from database.mariadb_session import get_db, SessionLocal
-
 
 class APIRequestParams(BaseModel):
     numOfRows: int
@@ -25,9 +21,7 @@ class APIRequestParams(BaseModel):
     contentTypeId: int
     serviceKey: str
 
-
 data_store: Dict[str, Any] = {}
-
 
 async def fetch_page(params: APIRequestParams) -> Dict[str, Any]:
     url = (
@@ -47,11 +41,8 @@ async def fetch_page(params: APIRequestParams) -> Dict[str, Any]:
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
             response.raise_for_status()
-
             xml_content = response.text
-
             parsed_data = xmltodict.parse(xml_content)
-
             return parsed_data
 
     except httpx.HTTPStatusError as e:
@@ -61,8 +52,7 @@ async def fetch_page(params: APIRequestParams) -> Dict[str, Any]:
     except Exception as e:
         return {"error": f"An error occurred: {e}"}
 
-
-async def getplace(mapX: float, mapY: float) -> Dict[int, Any]:
+async def getplace(mapX: float, mapY: float, contentTypeId: int) -> Dict[int, Any]:
     params = APIRequestParams(
         numOfRows=200,
         pageNo=1,
@@ -71,7 +61,7 @@ async def getplace(mapX: float, mapY: float) -> Dict[int, Any]:
         mapX=mapX,
         mapY=mapY,
         radius=20000,
-        contentTypeId=39,
+        contentTypeId=contentTypeId,
         serviceKey=settings.TOURAPI_LDM_KEY
     )
 
@@ -83,17 +73,14 @@ async def getplace(mapX: float, mapY: float) -> Dict[int, Any]:
         if 'error' in data:
             return {"message": "Failed to fetch data", "data": data}
 
-
         response = data.get('response', {})
         body = response.get('body', {})
 
         total_count = int(body.get('totalCount', 0))
         items = body.get('items', {}).get('item', [])
 
-
         all_data.extend(items)
         params.pageNo += 1
-
         page_limit = (total_count + params.numOfRows - 1) // params.numOfRows
 
     extracted_data = {}
@@ -113,36 +100,79 @@ async def getplace(mapX: float, mapY: float) -> Dict[int, Any]:
 
     return extracted_data
 
-
-async def main():
+async def save_place(spot_id: int):
     db: Session = SessionLocal()
     try:
-        spot_id = 10
-        spot_db = db.query(DronespotModel).filter(DronespotModel.id == spot_id).first()
-        if not spot_db:
-            print("Spot not found")
-            return
+        spot = db.query(DronespotModel).filter(DronespotModel.id == spot_id).first()
 
-        mapX = spot_db.lon
-        mapY = spot_db.lat
-        result = await getplace(mapX, mapY)  # 비동기 함수 호출
-        for index in range(1, len(result)):
-            place_data = PlaceModel(
-                name=result[index]['title'],
-                type=result[index]['contentid'],
-                lat=result[index]['mapy'],
-                lon=result[index]['mapx'],
-                address=result[index]['addr1'],
-                place_type_id=result[index]['contenttypeid']
-            )
-            db.add(place_data)
-            db.commit()
-            db.refresh(place_data)
+        # 이미 이 드론스팟에 대한 장소가 저장되어 있는지 확인
+        existing_places = db.query(DronePlaceModel).filter(DronePlaceModel.dronespot_id == spot_id).first()
+        if existing_places:
+            print(f"DroneSpot ID {spot_id}: 이미 장소가 저장되어 있습니다.")
+        else:
+            mapX = spot.lon
+            mapY = spot.lat
+            result_res = await getplace(mapX, mapY, 39)  # 비동기 함수 호출
+            result_accom = await getplace(mapX, mapY, 32)  # 비동기 함수 호출
 
-        print("완료")
+            for index in range(1, len(result_res) + 1):
+                place_data = PlaceModel(
+                    name=result_res[index]['title'],
+                    type=result_res[index]['contentid'],
+                    lat=result_res[index]['mapy'],
+                    lon=result_res[index]['mapx'],
+                    address=result_res[index]['addr1'],
+                    place_type_id=result_res[index]['contenttypeid']
+                )
+
+                db.add(place_data)
+                db.commit()
+                db.refresh(place_data)
+
+                # drone_place 테이블에 추가
+                drone_place_data = DronePlaceModel(
+                    dronespot_id=spot_id,
+                    place_id=place_data.id
+                )
+
+                db.add(drone_place_data)
+                db.commit()
+
+            for index in range(1, len(result_accom) + 1):
+                place_data = PlaceModel(
+                    name=result_accom[index]['title'],
+                    type=result_accom[index]['contentid'],
+                    lat=result_accom[index]['mapy'],
+                    lon=result_accom[index]['mapx'],
+                    address=result_accom[index]['addr1'],
+                    place_type_id=result_accom[index]['contenttypeid']
+                )
+
+                db.add(place_data)
+                db.commit()
+                db.refresh(place_data)
+
+                # drone_place 테이블에 추가
+                drone_place_data = DronePlaceModel(
+                    dronespot_id=spot_id,
+                    place_id=place_data.id
+                )
+
+                db.add(drone_place_data)
+                db.commit()
+
+            print(f"DroneSpot ID {spot_id}: 장소 저장 완료.")
+
+            # for spot in spot_list:
+            #     spot_id = spot.id
+            #
+            #     # 이미 이 드론스팟에 대한 장소가 저장되어 있는지 확인
+            #     existing_places = db.query(DronePlaceModel).filter(DronePlaceModel.dronespot_id == spot_id).first()
+            #     if existing_places:
+            #         print(f"DroneSpot ID {spot_id}: 이미 장소가 저장되어 있습니다. 건너뜁니다.")
+            #         continue
     finally:
-        db.close()  # 세션을 명시적으로 종료
+        db.close()
 
-
-if __name__ == "__main__":
-    asyncio.run(main())
+# if __name__ == "__main__":
+#     asyncio.run(save_place(10))
