471b64581e57281a484d924b274bc04fbf4b6135
K1A2 <jckim0307@gmail.com>
2024-08-24 16:47:29 +0900

    DRAV-123 feat: 로그인/로그아웃시 device_id 고려하도록 수정

--- api/v1/endpoints/logout.py
+++ api/v1/endpoints/logout.py
@@ -1,13 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, Request
+from sqlalchemy.orm import Session
+
 from schemas import Logout
 from starlette.responses import JSONResponse
+from models import Refresh as RefreshModel
+
+from core.security import decode_access_token
+
+from database.mariadb_session import get_db
 
 router = APIRouter()
 
 @router.post("/logout")
 async def logout(
-        logout_info: Logout
+        logout_info: Logout,
+        request: Request,
+        db: Session = Depends(get_db)
 ):
+    existing_refresh_token = db.query(RefreshModel).filter(
+        RefreshModel.uid == logout_info.uid,
+        RefreshModel.device_id == logout_info.device_id
+    ).first()
+
+    if existing_refresh_token:
+        db.delete(existing_refresh_token)
+        db.commit()
+
     response = JSONResponse(content={"message": "로그아웃 되었습니다."})
     response.delete_cookie(key="access_token")
     response.delete_cookie(key="refresh_token")
